159df3d06e593dd4ea635586d0ee1132f73a4efe
terrerox <terrero0611@gmail.com>
2024-01-21 16:53:47 -0400

    init

---
+++ CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,35 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by terrerox on 1/1/24.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(color: UIColor, title: String, systemImageName: String) {
+        super.init(frame: .zero)
+        
+        configuration = .tinted()
+        configuration?.title = title
+        configuration?.baseForegroundColor = color
+        configuration?.baseBackgroundColor = color
+        configuration?.cornerStyle = .medium
+        
+        configuration?.image = UIImage(systemName: systemImageName)
+        configuration?.imagePadding = 5
+        configuration?.imagePlacement = .trailing
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
